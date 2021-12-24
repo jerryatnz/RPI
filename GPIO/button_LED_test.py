10e868b04a623ef6591135da4835bdc1702aeace
jerryatnz <96602168+jerryatnz@users.noreply.github.com>
2021-12-23 23:55:38 -0800

    Update button_LED_test.py
    Tested on Pi 4, added comments in the code.

--- GPIO/button_LED_test.py
+++ GPIO/button_LED_test.py
@@ -3,10 +3,12 @@ from gpiozero import LED
 from gpiozero import Button
 from time import sleep
 
+#define physical connections on GPIO
 button = Button(6)
 led_y = LED(21)
 led_b = LED(26)
 
+#when button pressed, toggle LEDs accordingly, then wait for press again.
 while True:
     button.wait_for_press()
     led_y.on()
